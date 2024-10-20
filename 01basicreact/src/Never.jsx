3d6f58769ae048158b3568cdac58d3e63d176ea9
Rahul Kumar <rahulrawat03@outlook.com>
2024-10-20 11:29:54 +0530

    first 3 projects

---
+++ 01basicreact/src/Never.jsx
@@ -0,0 +1,16 @@
+// function App() {
+//     return (
+//       <h1> Coming soon... </h1>
+//     );
+//   }
+  
+//   export default App;
+
+
+function Hello(){
+    return (
+        <h1> last 90 days </h1>
+    );
+}
+
+export default Hello;
